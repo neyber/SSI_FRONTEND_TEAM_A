14b6a518168bf93a3d240c50b224036a0fb4eee5
LinetTorrico <linet.torrico@dharbor.com>
2018-05-01 18:57:04 -0400

    variable global
    variable global

---
+++ src/app/shared/services/global.ts
@@ -0,0 +1,7 @@
+export const global = {
+  url: 'http://localhost:8080/ssi/',
+  create: '',
+  update: '',
+  delete: ''
+
+  };
--- src/app/shared/services/report.service.ts
+++ src/app/shared/services/report.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/map';
-import {Observable} from 'rxjs/Observable';
+import { global } from './global';
 
 
 
 @Injectable()
 export class ReportService {
   private headers;
-
+  userId = 1;
   constructor(private http: Http) {
 
     this.headers = new Headers({'Content-Type': 'application/json'});
   }
 
   private url = 'http://localhost:8080/ssi/workItems?userId=1';
+  private url_= global.url + 'workItems?' + this.userId;
 
   getReport(){
 
