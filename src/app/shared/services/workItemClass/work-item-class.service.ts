5611357f311ed77abcbee59d1dc3ed2695059838
LinetTorrico <linet.torrico@dharbor.com>
2018-05-02 21:40:11 -0400

    create
    create

--- src/app/shared/services/workItemClass/work-item-class.service.ts
+++ src/app/shared/services/workItemClass/work-item-class.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { global } from '../global';
-import {Headers, Http} from '@angular/http';
+import {Headers, Http, RequestOptions} from '@angular/http';
 import { WorkItemClass } from '../../../shared/models/workItemClass/WorkItemClass';
 
 @Injectable()
@@ -22,16 +22,9 @@ export class WorkItemClassService {
   }
   addWorkItemClassification(workItemC: WorkItemClass){
 
-    const json = JSON.stringify(workItemC);
-    console.log('Description.............', workItemC.name);
-    let aux  =  { description: json['description'],
-      name: json['name']
-    }
-    const  params = 'json=' + json;
-
-    const headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
-    console.log("los parametros enviados"+params);
-    return this.http.post(global.url + 'workItemClassifications', params, {headers: headers})
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(global.url + 'workItemClassifications', workItemC, options)
       .map(res => res.json());
   }
 }
