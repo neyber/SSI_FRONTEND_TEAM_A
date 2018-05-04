b4c069261f5beb34f7c56de74d00e60f4fd64ed4
liz <liz_salazars@hotmail.com>
2018-05-04 00:16:09 -0400

    UI Roles - List and Create

---
+++ src/app/personnel/role/role.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import {CreateRoleComponent} from './create-role/create-role.component';
+
+export const ROLE_ROUTES: Routes = [
+  {path: 'newRole', component: CreateRoleComponent}
+];
