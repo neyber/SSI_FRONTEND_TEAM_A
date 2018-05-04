072f80d6d8ab48f812f4944b82aa98d4ab1a9120
Neyber Rojas <neyber.rojas@dharbor.com>
2018-05-04 13:30:47 -0400

    Adding delete departament and window alert

--- src/app/personnel/department/view-department/view-department.component.ts
+++ src/app/personnel/department/view-department/view-department.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Department} from '../../../shared/models/Personnel/Department';
 import {DepartmentService} from '../../../shared/services/Personnel/department.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-department',
@@ -10,17 +11,25 @@ import {DepartmentService} from '../../../shared/services/Personnel/department.s
 export class ViewDepartmentComponent implements OnInit {
   departments: Department[];
 
-  constructor(private departmentService: DepartmentService) { }
+  constructor(private departmentService: DepartmentService, private router: Router) { }
 
   ngOnInit() {
     this.departmentService.getDepartments().subscribe(
       result => {
-        this.departments = result.data;
+        this.departments = result.data;  
       },
       error => {
         console.log('error');
       }
     );
+  }
+
+  deleteDepartment(departmentId) {
+    if(confirm("Are you sure to delete?")) {
+      this.departmentService.deleteDepartment(departmentId).subscribe(res => {
+        console.log('Deleted');
+      });
+    }
   }
 
 }
