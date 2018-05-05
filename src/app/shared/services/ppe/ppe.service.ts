ef2b7477ef5f7cf102b40ec399b353b182718fc2
wcolina <walker.cbdiv@gmail.com>
2018-05-05 10:03:46 -0400

    Updated service to manage errors for personal protection equipments.

--- src/app/shared/services/ppe/ppe.service.ts
+++ src/app/shared/services/ppe/ppe.service.ts
@@ -2,29 +2,30 @@ import { Injectable } from '@angular/core';
 import {Headers, Http, RequestOptions} from '@angular/http';
 import {global} from '../global';
 import {PpeRequest} from '../../models/ppe/PpeRequest';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class PpeService {
   private headers;
 
-  constructor(private http: Http) {
-    this.headers = new Headers({'Content-Type': 'application/json'});
+  constructor(private httpClient: HttpClient) {
+    this.headers = new HttpHeaders({'Content-Type': 'application/json'});
   }
 
-  getPpe() {
-    return this.http.get(global.url + 'ppes').map(res => res.json());
+  getPpe(): Observable<any> {
+    return this.httpClient.get(global.url + 'ppes').map(res => res);
   }
-  getPpeById(ppeId:number){
-    return this.http.get(global.url + 'ppes/' + ppeId).map(res => res.json());
+  getPpeById(ppeId:number): Observable<any>{
+    return this.httpClient.get(global.url + 'ppes/' + ppeId).map(res => res);
   }
-  addPpe(ppe: PpeRequest) {
-    const headers = new Headers({ 'Content-Type': 'application/json' });
-    const options = new RequestOptions({ headers: headers });
-    return this.http.post(global.url + 'ppes', ppe, options)
-      .map(res => res.json());
+  addPpe(ppe: PpeRequest): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.httpClient.post(global.url + 'ppes', ppe, { headers: headers })
+      .map(res => res);
   }
-  deletePpe(ppeClassificationId:number){
-    return this.http.delete(global.url + 'ppes/' + ppeClassificationId).map(res => res.json());
+  deletePpe(ppeClassificationId:number): Observable<any>{
+    return this.httpClient.delete(global.url + 'ppes/' + ppeClassificationId).map(res => res);
   }
 
 }
