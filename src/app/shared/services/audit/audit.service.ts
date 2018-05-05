2b05135fddb787d46b1107c850f560a9396dc941
Neyber Rojas <neyber@gmail.com>
2018-05-05 10:07:27 -0400

    Using http client for handling errors with interceptors

--- src/app/shared/services/audit/audit.service.ts
+++ src/app/shared/services/audit/audit.service.ts
@@ -1,41 +1,40 @@
 import { Injectable } from '@angular/core';
 import { global } from '../global';
-import {Headers, Http, RequestOptions} from '@angular/http';
 import {AuditRequest} from '../../models/audit/auditRequest';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuditService {
 
   private headers;
   
-  constructor(private http: Http) {
-    this.headers = new Headers({'Content-Type': 'application/json'});
+  constructor(private httpClient: HttpClient) {
+    this.headers = new HttpHeaders({'Content-Type': 'application/json'});
   }
 
-  getListAudit() {
-    return this.http.get(global.url + 'audits').map(res => res.json());
+  getListAudit(): Observable<any> {
+    return this.httpClient.get(global.url + 'audits').map(res => res);
   }
 
-  getAuditById(auditId: number) {
-    return this.http.get(global.url + 'audits/' + auditId).map(res => res.json());
+  getAuditById(auditId: number): Observable<any> {
+    return this.httpClient.get(global.url + 'audits/' + auditId).map(res => res);
   }
 
-  addAudit(audit: AuditRequest) {
-    const headers = new Headers({ 'Content-Type': 'application/json' });
-    const options = new RequestOptions({ headers: headers });
+  addAudit(audit: AuditRequest): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    return this.http.post(global.url + 'audits', audit, options).map(res => res.json());
+    return this.httpClient.post(global.url + 'audits', audit, {headers: headers}).map(res => res);
   }
 
-  deleteAudit(auditId: number) {
-    return this.http.delete(global.url + 'audits/' + auditId).map(res => res.json());
+  deleteAudit(auditId: number): Observable<any> {
+    return this.httpClient.delete(global.url + 'audits/' + auditId).map(res => res);
   }
 
-  updateAudit(auditId: number, audit: AuditRequest) {
-    const headers = new Headers({ 'Content-Type': 'application/json' });
-    const options = new RequestOptions({ headers: headers });
+  updateAudit(auditId: number, audit: AuditRequest): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    return this.http.put(global.url + 'audits/' + auditId, audit, options).map(res => res.json());
+    return this.httpClient.put(global.url + 'audits/' + auditId, audit, { headers: headers }).map(res => res);
   }
 
 }
