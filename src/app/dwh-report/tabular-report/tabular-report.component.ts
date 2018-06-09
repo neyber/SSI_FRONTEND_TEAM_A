ab5cf38e9f77ecd2dc2a6b48c6ae8ca80851ef19
Neyber Rojas <neyber@gmail.com>
2018-06-09 01:11:12 -0400

    Added tabular report component modifications, model and view report

--- src/app/dwh-report/tabular-report/tabular-report.component.ts
+++ src/app/dwh-report/tabular-report/tabular-report.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TabularReportRequest } from '../../shared/models/dwhReport/TabularReportRequest';
+import { Router } from '@angular/router';
+import { AccidentReportService } from '../../shared/services/accidentReport/accident-report.service';
 
 @Component({
   selector: 'app-tabular-report',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabularReportComponent implements OnInit {
 
-  constructor() { }
+  public tabularReport: TabularReportRequest;
+  tabular = [];
+
+  constructor(private accidentReportService: AccidentReportService, private router: Router) {
+    this.tabularReport = new TabularReportRequest('', '');
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.accidentReportService.getAccidentReportByRangeDate(this.tabularReport.fromDate, this.tabularReport.toDate).subscribe(
+      result => {
+        console.log(result);
+        
+        this.tabular = result.map(result => result);
+        
+        // let genders = result.map(result => result.gender);
+        // let roles = result.map(result => result.role);
+        //let quantities = result.map(result => result.quantity);
+        // let years = result.map(result => result.year);
+        // let months = result.map(result => result.month);
+        // let days = result.map(result => result.day);
+        //let fullDates = result.map(result => result.fullDate);
+      },
+      error => {
+        console.log('error');
+      }
+    );
   }
 
 }
